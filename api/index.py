8dd7c6b2ad6b85af5e441fccb84ef0e3462e0223
morph <morpheus17201@gmail.com>
2025-01-20 19:55:15 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
 
 class handler(BaseHTTPRequestHandler):
 
@@ -6,8 +7,10 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
+        url = self.path
+        parsed = parse_qs(urlparse(url).query)
 
         with open('q-vercel-python.json', 'r') as f:
             for line in f:
-                self.wfile.write(line.encode('utf-8'))
+                self.wfile.write(str(parsed).encode('utf-8'))
         return
